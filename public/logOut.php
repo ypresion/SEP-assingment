03cddf3c23725102581669f21f457d49ede5923e
tashboi <W18011022@northumbria.ac.uk>
2020-04-23 15:25:15 +0100

    All Working files
    merged Tom and dammy php/html
    
    added nav links at bottom
    
    http://unn-w18011022.newnumyspace.co.uk/SEP-assingment/public/index.php

--- public/logOut.php
+++ public/logOut.php
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
+<?php
+ini_set("session.save_path", "/home/unn_w18011022/sessionData");
+session_start();
+
+$_SESSION = array();
+
+session_destroy();
+
+header("Location: http://unn-w18011022.newnumyspace.co.uk/SEP-assingment/public/index.php");
+=======
 <?php
 ini_set("session.save_path", "/home/unn_w18011022/sessionData");
 session_start();
@@ -7,3 +18,4 @@ $_SESSION = array();
 session_destroy();
 
 header("Location: http://unn-w18011022.newnumyspace.co.uk/SEP-assingment/public/Index.php");
+>>>>>>> 2122a581256b4fb75eceadf70b54ed964e2af337
