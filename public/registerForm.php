aa1a8f56da46b30b5c04edaa5b4c75231ca78028
tashboi <w18011022@northumbria.ac.uk>
2020-04-23 16:19:08 +0100

    Changed Button text

--- public/registerForm.php
+++ public/registerForm.php
@@ -110,11 +110,12 @@ require_once("functions.php");
 								</div>
 							</div>
 							<div class="mb-6 text-center">
-								<input
+								<button
 									class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
 									type="submit"
 								>
-								</input>
+                                    Register!
+								</button>
 							</div>
 							<hr class="mb-6 border-t" />
 							<div class="text-center">
