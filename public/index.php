75002f59daf8d205afbf9238ed3875f00d3c237f
tashboi <w18011022@northumbria.ac.uk>
2020-05-13 17:44:11 +0100

    smol changes

--- public/index.php
+++ public/index.php
@@ -112,6 +112,7 @@ if($queryResult === false) {
     </nav>
 
 
+<!-- Hero Section -->
 <div class="carousel relative container mx-auto" style="max-width:2560px;">
     <div class="carousel-inner relative overflow-hidden w-full">
         <!--Slide 1-->
@@ -120,8 +121,8 @@ if($queryResult === false) {
             <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style="background-image: url('assets/images/d5.jpg');">
                 <div class="container mx-auto">
                     <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-                        <p class="text-white text-2xl font-bold my-4">Best Selling Gaming PC</p>
-                        <a class=" text-white text-xl inline-block no-underline border-2 p-2 uppercase border-gray-600 leading-relaxed hover:text-yellow-400 hover:border-yellow-400" href="#">view product</a>
+                        <p class="text-white text-2xl font-bold my-4">Best Selling Gaming PC's</p>
+                        <a class=" text-white text-xl inline-block no-underline border-2 p-2 uppercase border-gray-600 leading-relaxed hover:text-yellow-400 hover:border-yellow-400" href="product.php">view products</a>
                     </div>
                 </div>
 
@@ -137,7 +138,7 @@ if($queryResult === false) {
                 <div class="container mx-auto">
                     <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
                         <p class="text-white text-2xl font-bold my-4">Best Selling Laptops</p>
-                        <a class=" text-white text-xl inline-block no-underline border-2 p-2 uppercase border-gray-600 leading-relaxed hover:text-yellow-400 hover:border-yellow-400" href="#">view product</a>
+                        <a class=" text-white text-xl inline-block no-underline border-2 p-2 uppercase border-gray-600 leading-relaxed hover:text-yellow-400 hover:border-yellow-400" href="product.php">view products</a>
                     </div>
                 </div>
 
@@ -153,73 +154,7 @@ if($queryResult === false) {
                 <div class="container mx-auto">
                     <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
                         <p class="text-white text-2xl font-bold my-4">Best Selling Accessories</p>
-                        <a class=" text-white text-xl inline-block no-underline border-2 p-2 uppercase border-gray-600 leading-relaxed hover:text-yellow-400 hover:border-yellow-400" href="#">view product</a>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-        <label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-        <!-- Add additional indicators for each slide-->
-        <ol class="carousel-indicators">
-            <li class="inline-block mr-3">
-                <label for="carousel-1" class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
-            </li>
-            <li class="inline-block mr-3">
-                <label for="carousel-2" class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
-            </li>
-            <li class="inline-block mr-3">
-                <label for="carousel-3" class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
-            </li>
-        </ol>
-
-    </div>
-</div>
-<!-- End of Hero -->
-<div class="carousel relative container mx-auto" style="max-width:2560px;">
-    <div class="carousel-inner relative overflow-hidden w-full">
-        <!--Slide 1-->
-        <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
-        <div class="carousel-item absolute opacity-0" style="height:50vh;">
-            <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style="background-image: url('assets/images/d5.jpg');">
-                <div class="container mx-auto">
-                    <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-                        <p class="text-white text-2xl font-bold my-4">Best Selling Gaming PC</p>
-                        <a class=" text-white text-xl inline-block no-underline border-2 p-2 uppercase border-gray-600 leading-relaxed hover:text-yellow-400 hover:border-yellow-400" href="#">view product</a>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-        <label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-        <!--Slide 2-->
-        <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
-        <div class="carousel-item absolute opacity-0 bg-cover bg-right" style="height:50vh;">
-            <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style="background-image: url('assets/images/d4.jpg');">
-                <div class="container mx-auto">
-                    <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-                        <p class="text-white text-2xl font-bold my-4">Best Selling Laptops</p>
-                        <a class=" text-white text-xl inline-block no-underline border-2 p-2 uppercase border-gray-600 leading-relaxed hover:text-yellow-400 hover:border-yellow-400" href="#">view product</a>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-        <label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-        <!--Slide 3-->
-        <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
-        <div class="carousel-item absolute opacity-0" style="height:50vh;">
-            <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style="background-image: url('assets/images/d1.png');">
-                <div class="container mx-auto">
-                    <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-                        <p class="text-white text-2xl font-bold my-4">Best Selling Accessories</p>
-                        <a class=" text-white text-xl inline-block no-underline border-2 p-2 uppercase border-gray-600 leading-relaxed hover:text-yellow-400 hover:border-yellow-400" href="#">view product</a>
+                        <a class=" text-white text-xl inline-block no-underline border-2 p-2 uppercase border-gray-600 leading-relaxed hover:text-yellow-400 hover:border-yellow-400" href="product.php">view products</a>
                     </div>
                 </div>
 
@@ -248,17 +183,17 @@ if($queryResult === false) {
 <div class="flex flex-col lg:flex-row  mt-2">
     <div class="w-full lg:w-1/3 h-auto p-2  md:text-xl ">
         <button class="w-full p-2 bg-gray-800 hover:bg-gray-900 text-white font-semibold hover:text-white  border border-gray-400 hover:border-transparent rounded">
-            <a href="#">SHOP ACCESSORIES</a>
+            <a href="product.php">SHOP ACCESSORIES</a>
         </button>
     </div>
     <div class="w-full lg:w-1/3 h-auto p-2  md:text-xl ">
         <button class="w-full p-2 bg-gray-800 hover:bg-gray-900 text-white font-semibold hover:text-white  border border-gray-400 hover:border-transparent rounded">
-            <a href="#">SHOP GAMES</a>
+            <a href="product.php">SHOP GAMES</a>
         </button>
     </div>
     <div class="w-full lg:w-1/3 h-auto p-2  md:text-xl ">
         <button class="w-full p-2 bg-gray-800 hover:bg-gray-9I00 text-white font-semibold hover:text-white  border border-gray-400 hover:border-transparent rounded">
-            <a href="#">SHOP CONTROLLERS</a>
+            <a href="product.php">SHOP CONTROLLERS</a>
         </button>
     </div>
 
