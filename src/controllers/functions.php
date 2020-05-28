623d957fa1ca956f55aa211eeb2056a18a1e56b5
ypresion <45670033+ypresion@users.noreply.github.com>
2020-05-28 20:22:08 +0100

    Update functions.php

--- src/controllers/functions.php
+++ src/controllers/functions.php
@@ -2,8 +2,8 @@
 <?php
 function getConnection() {
     try {
-        $connection = new PDO("mysql:host=localhost;dbname=unn_w18011022",
-            "unn_w18011022", "OUNEQTFE");
+        $connection = new PDO("mysql:host=localhost;dbname=",
+            "", "");
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $connection;
     } catch (Exception $e) {
